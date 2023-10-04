f1788b65ace05e1f52446e5d8ff97988a900d82e
MarsIfeanyi <marschalice@gmail.com>
2023-10-04 05:01:29 +0100

    Update test file

--- test/BridgeWatersMarketplace.t.sol
+++ test/BridgeWatersMarketplace.t.sol
@@ -7,17 +7,29 @@ import {BridgeWatersMarketplace} from "../src/BridgeWatersMarketplace.sol";
 contract BridgeWatersMarketplaceTest is Test {
     BridgeWatersMarketplace public bridgeWaters;
 
+    address tokenAddress = 0x06448574948e481E395C702B65e13bB23C2f5aeF;
+    uint256 tokenId;
+    uint256 price = 0.05 ether;
+    uint256 deadline = block.timestamp + 1 days;
+    bytes signature;
+    uint256 itemsCounter;
+
     function setUp() public {
         bridgeWaters = new BridgeWatersMarketplace();
     }
 
-    function test_CreateOrder(
+    function test_ListItem(
         address _tokenAddress,
         uint256 _tokenId,
         uint256 _price,
         uint256 _deadline,
         bytes memory _signature
-    ) public {}
+    ) public {
+        BridgeWatersMarketplace.ItemInfo storage itemInfo = bridgeWaters
+            .listedItems[itemsCounter];
 
-    function test_BuyOrder(uint256 _orderId) public {}
+        assertEq(bridgeWaters.ItemInfo.tokenAddress(), _tokenAddress);
+    }
+
+    function test_BuyItem(uint256 _orderId) public {}
 }
