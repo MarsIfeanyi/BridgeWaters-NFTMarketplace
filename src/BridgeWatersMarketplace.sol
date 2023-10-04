525e681f7c2d9af348935679805a93583951b565
MarsIfeanyi <marschalice@gmail.com>
2023-10-04 02:42:08 +0100

    Update Marketplace contract

--- src/BridgeWatersMarketplace.sol
+++ src/BridgeWatersMarketplace.sol
@@ -6,22 +6,34 @@ import "@openzeppelin/contracts/utils/cryptography/ECDSA.sol";
 import "@openzeppelin/contracts/utils/Address.sol";
 
 contract BridgeWatersMarketplace {
-    struct OrderInfo {
+    struct ItemInfo {
         address payable owner;
         address tokenAddress;
         uint256 tokenId;
         uint256 price;
         bytes signature;
         uint256 deadline;
-        bool isSold;
+        bool isActive;
     }
 
-    uint256 public odersCounter;
-    mapping(uint256 odersCounter => OrderInfo) public listedOrders;
+    uint256 public itemsCounter;
+    mapping(uint256 itemsCounter => ItemInfo) public listedItems;
 
-    constructor() {}
+    event ItemListed(
+        address owner,
+        uint256 indexed _tokenId,
+        address indexed _tokenAddress,
+        uint256 indexed _price,
+        uint256 _deadline
+    );
 
-    function getOrderHash(
+    event ItemSold(
+        address indexed owner,
+        uint256 indexed tokenId,
+        uint256 indexed price
+    );
+
+    function getSigHash(
         address _tokenAddress,
         address _owner,
         uint256 _tokenId,
@@ -41,97 +53,101 @@ contract BridgeWatersMarketplace {
     }
 
     function verifySignature(
-        bytes32 _orderHash,
+        bytes32 _sigHash,
         bytes memory _signature,
         address _expectedSigner
     ) public pure returns (bool) {
-        return ECDSA.recover(_orderHash, _signature) == _expectedSigner;
+        return ECDSA.recover(_sigHash, _signature) == _expectedSigner;
     }
 
-    function createOrder(
+    function listItem(
         address _tokenAddress,
         uint256 _tokenId,
         uint256 _price,
         uint256 _deadline,
         bytes memory _signature
     ) public {
-        // address owner = msg.sender;
-        // require(owner != address(0), "Invalid address");
-        require(_tokenAddress != address(0), "Invalid tokenAddress");
-        require(_price != 0, "Wrong Price");
-        require(_tokenId != 0, "Invalid tokenId");
-        // todo: check that the creator is the owner
+        // checks
         require(
             msg.sender == IERC721(_tokenAddress).ownerOf(_tokenId),
             "Not owner"
         );
+        require(
+            IERC721(_tokenAddress).isApprovedForAll(msg.sender, address(this)),
+            "ERC721: Insufficient Approval"
+        );
+        require(_tokenAddress != address(0), "Invalid tokenAddress");
+        require(_price > 0, "Zero Price Not Allowed");
+        require(_tokenId != 0, "Invalid tokenId");
+        require(
+            _deadline > block.timestamp + 1 days,
+            "Deadline should be in the future"
+        );
 
-        // bytes32 orderHash = getOrderHash(
-        //     _tokenAddress,
-        //     address(owner),
-        //     _tokenId,
-        //     _price,
-        //     _deadline
-        // );
-        // require(
-        //     verifySignature(orderHash, _signature, msg.sender),
-        //     "Invalid signature"
-        // );
+        // increment counter
+        itemsCounter += 1;
 
-        odersCounter += 1;
-
-        // TODO: Add check for approvals... Approval for all
-
+        // transfer the NFT from owner to marketplace
         IERC721(_tokenAddress).transferFrom(
             msg.sender,
             address(this),
             _tokenId
         );
 
-        OrderInfo storage orderinfo = listedOrders[odersCounter];
-        orderinfo.tokenId = _tokenId;
-        orderinfo.tokenAddress = _tokenAddress;
-        orderinfo.deadline = _deadline;
-        orderinfo.owner = payable(msg.sender);
-        orderinfo.price = _price;
-        orderinfo.signature = _signature;
+        // Access storage loaction
+        ItemInfo storage itemInfo = listedItems[itemsCounter];
+
+        // Update the struct mapping
+        itemInfo.tokenId = _tokenId;
+        itemInfo.tokenAddress = _tokenAddress;
+        itemInfo.deadline = _deadline;
+        itemInfo.owner = payable(msg.sender);
+        itemInfo.price = _price;
+        itemInfo.signature = _signature;
+        itemInfo.isActive = true;
+
+        // emit event
+        emit ItemListed(msg.sender, _tokenId, _tokenAddress, _price, _deadline);
     }
 
-    function buyOrder(uint256 _orderId) external payable {
-        require(_orderId != 0 && _orderId <= odersCounter, "Wrong _orderId");
+    function buyItem(uint256 _listingId) external payable {
+        require(
+            _listingId != 0 && _listingId <= itemsCounter,
+            "Wrong _listingId"
+        );
 
-        OrderInfo storage orderinfo = listedOrders[_orderId];
+        ItemInfo storage itemInfo = listedItems[_listingId];
 
         require(
-            block.timestamp <= orderinfo.deadline,
+            block.timestamp <= itemInfo.deadline,
             "Order deadline has passed"
         );
-        require(msg.value == orderinfo.price, "Wrong ETH was sent");
-        require(!orderinfo.isSold, "NFT-Order is already sold");
+        require(msg.value == itemInfo.price, "Wrong ETH was sent");
+        require(!itemInfo.isActive, "NFT-Order is already sold");
 
-        bytes32 orderHash = getOrderHash(
-            orderinfo.tokenAddress,
-            orderinfo.owner,
-            orderinfo.tokenId,
-            orderinfo.price,
-            orderinfo.deadline
+        bytes32 sigHash = getSigHash(
+            itemInfo.tokenAddress,
+            itemInfo.owner,
+            itemInfo.tokenId,
+            itemInfo.price,
+            itemInfo.deadline
         );
         require(
-            verifySignature(orderHash, orderinfo.signature, orderinfo.owner),
+            verifySignature(sigHash, itemInfo.signature, itemInfo.owner),
             "Invalid signature"
         );
 
-        orderinfo.isSold = true;
+        itemInfo.isActive = true;
 
-        IERC721(orderinfo.tokenAddress).transferFrom(
+        (bool sent, ) = itemInfo.owner.call{value: msg.value}("");
+        require(sent, "Failed to transfer ETH");
+
+        IERC721(itemInfo.tokenAddress).transferFrom(
             address(this),
             msg.sender,
-            orderinfo.tokenId
+            itemInfo.tokenId
         );
 
-        (bool sent, ) = orderinfo.owner.call{value: msg.value}("");
-        require(sent, "Failed to transfer ETH");
+        emit ItemSold(msg.sender, itemInfo.tokenId, msg.value);
     }
 }
-
-// use the sign cheatcode
