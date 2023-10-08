f9adc09dfb569a93b9ee15a4c1f9440ef1c9f534
MarsIfeanyi <marschalice@gmail.com>
2023-10-08 23:25:38 +0100

    Create more test cases

--- test/BridgeWatersMarketplace.t.sol
+++ test/BridgeWatersMarketplace.t.sol
@@ -20,10 +20,25 @@ contract BridgeWatersMarketplaceTest is Helpers {
     uint256 privateKey2;
     uint256 privateKey3;
 
+    struct ListingInfo {
+        address token;
+        uint256 tokenId;
+        uint256 price;
+        bytes signature;
+        // slot packing... Slot 4
+        uint256 deadline;
+        address seller;
+        bool isActive;
+    }
+
     // getting and declaring the struct state variable
     BridgeWatersMarketplace.ListingInfo listingInfo;
 
     // BridgeWatersMarketplace.listingsInfo[currentListingId];
+
+    event ListingCreated(uint256 indexed listingId);
+    event ListingExecuted(uint256 indexed listingId, ListingInfo);
+    event ListingEdited(uint256 indexed listingId, ListingInfo);
 
     function setUp() public {
         bridgeWaters = new BridgeWatersMarketplace();
@@ -35,36 +50,64 @@ contract BridgeWatersMarketplaceTest is Helpers {
         (creator2, privateKey2) = mkaddr("CREATOR2");
         (spender, privateKey3) = mkaddr("SPENDER");
 
-        // Accessing the data is the struct and saving it to the state variable
-        // _listingInfo = _BridgeWatersMarketplace._ListingInfo({
-        //     token: address(bridgeWaterNFT),
-        //     tokenId: 1,
-        //     price: 1 ether,
-        //     signature: bytes(""),
-        //     deadline: 0,
-        //     seller: address(0),
-        //     isActive: false
-        // });
+        bridgeWaterNFT.mint(creator1, 2);
 
         listingInfo.token = address(bridgeWaterNFT);
-        listingInfo.tokenId = 1;
-        listingInfo.price = 1 ether;
-        listingInfo.signature = bytes("");
-        listingInfo.deadline = 0;
-        listingInfo.seller = address(0);
-        listingInfo.isActive = false;
+        listingInfo.tokenId = 2;
+        listingInfo.price = 2 ether;
+        listingInfo.deadline = 2 days;
+        listingInfo.seller;
+        listingInfo.isActive = true;
+        listingInfo.signature;
+    }
 
-        bridgeWaterNFT.mint(creator1, 1);
+    function testCreateListing() public {
+        // vm.startPrank(creator1);
+        switchSigner(creator1);
+        //bridgeWaterNFT.mint(creator1, 2);
+        bridgeWaterNFT.setApprovalForAll(address(bridgeWaters), true);
+
+        bytes memory _signature = constructSig(
+            address(bridgeWaterNFT),
+            2,
+            2 ether,
+            2 days,
+            creator1,
+            privateKey1
+        );
+        listingInfo.signature = _signature;
+        listingInfo.seller = creator1;
+        listingInfo.tokenId = 2;
+
+        currentListingId = bridgeWaters.createListing(
+            listingInfo.token,
+            listingInfo.tokenId,
+            listingInfo.price,
+            listingInfo.signature,
+            listingInfo.deadline,
+            listingInfo.seller,
+            listingInfo.isActive = true
+        );
+
+        assertEq(listingInfo.token, address(bridgeWaterNFT));
+        assertEq(listingInfo.tokenId, 2);
+        assertEq(listingInfo.price, 2 ether);
+        assertEq(listingInfo.signature, _signature);
+        assertEq(listingInfo.deadline, 2 days);
+        assertEq(listingInfo.seller, creator1);
+        assertEq(listingInfo.isActive, true);
     }
 
     function _createListing() internal returns (uint256 _listingId) {
+        // listingInfo.seller = creator1;
         _listingId = bridgeWaters.createListing(
             listingInfo.token,
             listingInfo.tokenId,
             listingInfo.price,
             listingInfo.signature,
             listingInfo.deadline,
-            listingInfo.seller
+            listingInfo.seller,
+            listingInfo.isActive
         );
     }
 
@@ -96,11 +139,33 @@ contract BridgeWatersMarketplaceTest is Helpers {
 
     function testCreateListing_DeadlineTooSoon() public {
         switchSigner(creator1);
-
+        //bridgeWaterNFT.mint(creator1, 2);
         bridgeWaterNFT.setApprovalForAll(address(bridgeWaters), true);
 
+        bytes memory _signature = constructSig(
+            address(bridgeWaterNFT),
+            2,
+            1 ether,
+            2 days,
+            creator1,
+            privateKey1
+        );
+        // vm.warp(2 days);
+        listingInfo.signature = _signature;
+        listingInfo.seller = creator1;
+        listingInfo.tokenId = 2;
+        listingInfo.deadline = 0 minutes;
+
         vm.expectRevert(BridgeWatersMarketplace.DeadlineTooSoon.selector);
-        _createListing();
+        bridgeWaters.createListing(
+            listingInfo.token,
+            listingInfo.tokenId,
+            listingInfo.price,
+            listingInfo.signature,
+            listingInfo.deadline,
+            listingInfo.seller,
+            listingInfo.isActive
+        );
     }
 
     function testCreateListing_MinimumDurationNotMet() public {
@@ -108,7 +173,7 @@ contract BridgeWatersMarketplaceTest is Helpers {
 
         bridgeWaterNFT.setApprovalForAll(address(bridgeWaters), true);
 
-        listingInfo.deadline = uint88(block.timestamp + 30 minutes);
+        listingInfo.deadline = 30 minutes;
 
         vm.expectRevert(BridgeWatersMarketplace.MinDurationNotMet.selector);
         _createListing();
@@ -120,45 +185,309 @@ contract BridgeWatersMarketplaceTest is Helpers {
         bridgeWaterNFT.setApprovalForAll(address(bridgeWaters), true);
 
         listingInfo.deadline = uint88(block.timestamp + 3 hours);
-
-        listingInfo.signature = constructSig(
+        listingInfo.seller = creator1;
+        bytes memory _signature = constructSig(
+            address(bridgeWaterNFT),
+            2,
+            3 ether,
+            3 days,
+            creator1,
+            privateKey1
+        );
+        listingInfo.signature = _signature;
+        vm.expectRevert(BridgeWatersMarketplace.InValidSignature.selector);
+        bridgeWaters.createListing(
             listingInfo.token,
             listingInfo.tokenId,
             listingInfo.price,
+            listingInfo.signature,
             listingInfo.deadline,
             listingInfo.seller,
-            privateKey2
+            listingInfo.isActive
         );
-
-        vm.expectRevert(BridgeWatersMarketplace.InValidSignature.selector);
-        _createListing();
     }
 
-    function testCreateListing() public {
-        bridgeWaterNFT.setApprovalForAll(address(bridgeWaters), true);
+    // function _testCreateListing_Emitevent_ListingCreated() public {
+    //  switchSigner(creator1);
 
-        _createListing();
+    //     vm.expectEmit();
 
-        assertEq(listingInfo.token, address(bridgeWaterNFT));
-        assertEq(listingInfo.tokenId, 1);
-        assertEq(listingInfo.price, 1 ether);
-        assertEq(listingInfo.signature, bytes(""));
-        assertEq(listingInfo.deadline, 0);
-        assertEq(listingInfo.seller, address(0));
-        assertEq(listingInfo.isActive, false);
-    }
+    //     emit ListingCreated(currentListingId);
+
+    //     _createListing();
+    // }
 
     // executeListing
 
-    function _executeListing() internal {
-        bridgeWaters.executeListing(4);
+    function _buyListing() internal {
+        switchSigner(creator1);
+        bridgeWaters.buyListing(currentListingId);
     }
 
-    function testExecuteListing_ListingDoesNotExist() public {
-        switchSigner(creator1);
+    function testBuyListing_ListingDoesNotExist() public {
+        testCreateListing();
 
         vm.expectRevert(BridgeWatersMarketplace.ListingDoesNotExist.selector);
 
-        _executeListing();
+        _buyListing();
     }
+
+    function testBuyListing_ListingExpired() public {
+        testCreateListing();
+
+        vm.warp(1641070800);
+
+        vm.expectRevert(BridgeWatersMarketplace.ListingExpired.selector);
+        // When listing expires, the listingId becomes zero
+
+        bridgeWaters.buyListing(0);
+    }
+
+    function testBuyListing_ListingNotActive() public {
+        switchSigner(creator1);
+        // bridgeWaterNFT.mint(creator1, 2);
+        bridgeWaterNFT.setApprovalForAll(address(bridgeWaters), true);
+
+        bytes memory _signature = constructSig(
+            address(bridgeWaterNFT),
+            2,
+            2 ether,
+            2 days,
+            creator1,
+            privateKey1
+        );
+        listingInfo.signature = _signature;
+        listingInfo.seller = creator1;
+        listingInfo.tokenId = 2;
+
+        bridgeWaters.createListing(
+            listingInfo.token,
+            listingInfo.tokenId,
+            listingInfo.price,
+            listingInfo.signature,
+            listingInfo.deadline,
+            listingInfo.seller,
+            listingInfo.isActive = false
+        );
+
+        // vm.warp(1641070800);
+
+        vm.expectRevert(BridgeWatersMarketplace.ListingNotActive.selector);
+        // bridgeWaters.buyListing(_currentListingId);
+
+        _buyListing();
+    }
+
+    function testBuyListing_PriceMismatch() public {
+        _createBuyListing();
+
+        vm.expectRevert(
+            abi.encodeWithSelector(
+                BridgeWatersMarketplace.PriceMismatch.selector,
+                listingInfo.price
+            )
+        );
+
+        bridgeWaters.buyListing{value: 3 ether}(currentListingId);
+    }
+
+    function testBuyListing_PriceNotMet() public {
+        _createBuyListing();
+
+        vm.expectRevert(
+            abi.encodeWithSelector(
+                BridgeWatersMarketplace.PriceNotMet.selector,
+                (listingInfo.price - 1 ether)
+            )
+        );
+
+        bridgeWaters.buyListing{value: 1 ether}(currentListingId);
+    }
+
+    function _createBuyListing() internal {
+        switchSigner(creator1);
+        // bridgeWaterNFT.mint(creator1, 2);
+        bridgeWaterNFT.setApprovalForAll(address(bridgeWaters), true);
+
+        bytes memory _signature = constructSig(
+            address(bridgeWaterNFT),
+            2,
+            2 ether,
+            2 days,
+            creator1,
+            privateKey1
+        );
+        listingInfo.signature = _signature;
+        listingInfo.seller = creator1;
+        listingInfo.tokenId = 2;
+
+        bridgeWaters.createListing(
+            listingInfo.token,
+            listingInfo.tokenId,
+            listingInfo.price,
+            listingInfo.signature,
+            listingInfo.deadline,
+            listingInfo.seller,
+            listingInfo.isActive
+        );
+    }
+
+    // UPDATE LISTING
+
+    function _updateListing() internal {
+        switchSigner(creator1);
+        bridgeWaters.updateListing(
+            currentListingId,
+            3 ether,
+            listingInfo.isActive
+        );
+    }
+
+    function testUpdateListing_ListingDoesNotExist() public {
+        switchSigner(creator1);
+        // bridgeWaterNFT.mint(creator1, 2);
+        bridgeWaterNFT.setApprovalForAll(address(bridgeWaters), true);
+
+        bytes memory _signature = constructSig(
+            address(bridgeWaterNFT),
+            2,
+            2 ether,
+            2 days,
+            creator1,
+            privateKey1
+        );
+        listingInfo.signature = _signature;
+        listingInfo.seller = creator1;
+        listingInfo.tokenId = 2;
+
+        currentListingId = bridgeWaters.createListing(
+            listingInfo.token,
+            listingInfo.tokenId,
+            listingInfo.price,
+            listingInfo.signature,
+            listingInfo.deadline,
+            listingInfo.seller,
+            listingInfo.isActive
+        );
+
+        vm.expectRevert(BridgeWatersMarketplace.ListingDoesNotExist.selector);
+
+        bridgeWaters.updateListing(currentListingId, 0, false);
+    }
+
+    function testUpdateListing_NotOwner() public {
+        switchSigner(creator1);
+        bridgeWaterNFT.mint(creator1, 3);
+        bridgeWaterNFT.setApprovalForAll(address(bridgeWaters), true);
+
+        bytes memory _signature = constructSig(
+            address(bridgeWaterNFT),
+            2,
+            2 ether,
+            2 days,
+            creator1,
+            privateKey1
+        );
+        listingInfo.signature = _signature;
+        listingInfo.seller = creator1;
+        //listingInfo.tokenId
+
+        uint256 _currentListingId = bridgeWaters.createListing(
+            listingInfo.token,
+            listingInfo.tokenId,
+            listingInfo.price,
+            listingInfo.signature,
+            listingInfo.deadline,
+            listingInfo.seller,
+            listingInfo.isActive
+        );
+
+        vm.startPrank(spender);
+
+        vm.expectRevert(BridgeWatersMarketplace.NotOwner.selector);
+
+        bridgeWaters.updateListing(_currentListingId, 1 ether, true);
+    }
+
+    // function testUpdateListing() public {
+    //     listingInfo.seller = creator1;
+
+    //     switchSigner(creator1);
+    //     //bridgeWaterNFT.mint(creator1, 2);
+    //     bridgeWaterNFT.setApprovalForAll(address(bridgeWaters), true);
+
+    //     bytes memory _signature = constructSig(
+    //         address(bridgeWaterNFT),
+    //         2,
+    //         2 ether,
+    //         2 days,
+    //         creator1,
+    //         privateKey1
+    //     );
+    //     listingInfo.signature = _signature;
+
+    //     listingInfo.tokenId = 2;
+
+    //     currentListingId = bridgeWaters.createListing(
+    //         listingInfo.token,
+    //         listingInfo.tokenId,
+    //         listingInfo.price,
+    //         listingInfo.signature,
+    //         listingInfo.deadline,
+    //         listingInfo.seller,
+    //         listingInfo.isActive
+    //     );
+
+    //     uint256 newPrice = 3 ether;
+
+    //     assertEq(
+    //         bridgeWaterNFT.ownerOf(listingInfo.tokenId),
+    //         address(bridgeWaters)
+    //     );
+
+    //     bridgeWaters.updateListing(
+    //         currentListingId,
+    //         newPrice,
+    //         listingInfo.isActive
+    //     );
+
+    //     listingInfo = bridgeWaters.getListing(currentListingId);
+
+    //     assertEq(listingInfo.price, newPrice);
+    //     assertEq(listingInfo.isActive, true);
+    // }
+
+    // function testBuyListing() public {
+    //         switchSigner(creator1);
+
+    //         vm.warp(1641070800);
+
+    //         listingInfo.deadline = uint88(block.timestamp + 120 minutes);
+
+    //         bridgeWaterNFT.setApprovalForAll(address(bridgeWaters), true);
+
+    //         listingInfo.signature = constructSig(
+    //             listingInfo.token,
+    //             listingInfo.tokenId,
+    //             listingInfo.price,
+    //             listingInfo.deadline,
+    //             listingInfo.seller,
+    //             privateKey1
+    //         );
+
+    //         currentListingId = bridgeWaters.createListing(
+    //             listingInfo.token,
+    //             listingInfo.tokenId,
+    //             listingInfo.price,
+    //             listingInfo.signature,
+    //             listingInfo.deadline,
+    //             listingInfo.seller,
+    //             listingInfo.isActive
+    //         );
+
+    //         switchSigner(spender);
+    //         //vm.warp(1641070800);
+    //         bridgeWaters.buyListing{value: listingInfo.price}(currentListingId);
+    //         assertEq(bridgeWaterNFT.ownerOf(currentListingId), spender);
+    //     }
 }
